3107e09c81e43fc678ad862e0713cee337d4df62
bagus <54665358+bagus2x@users.noreply.github.com>
2020-12-03 00:59:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/*', (_, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
